c7a812258b323ce89acc118d9061cab900dbe3a4
rg <ritobroto11@gmail.com>
2021-07-26 17:04:06 +0530

    Added navigation drawer

--- Categories/app/src/main/java/com/rgdgr8/categories/MainActivity.java
+++ Categories/app/src/main/java/com/rgdgr8/categories/MainActivity.java
@@ -1,32 +1,66 @@
 package com.rgdgr8.categories;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setTitle("");
+        /*getSupportActionBar().setTitle("");
         getSupportActionBar().setElevation(0);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.drawer_icon);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);*/
 
         setContentView(R.layout.activity_main);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         Fragment fragment = fragmentManager.findFragmentById(R.id.frame);
 
-        if(fragment==null){
+        if (fragment == null) {
             fragment = new MainFragment();
             fragmentManager.beginTransaction()
-                    .add(R.id.frame,fragment)
+                    .add(R.id.frame, fragment)
                     .commit();
         }
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        //toolbar.setNavigationIcon(R.drawable.drawer_icon);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        /*ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();*/
+
+        ImageView drawerIcon = findViewById(R.id.drawer_icon);
+        drawerIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
     }
 }
